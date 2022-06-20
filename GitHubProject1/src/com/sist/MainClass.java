71da2e49e7cef3ea91280a8c4a3d20608098e404
a <a@DESKTOP-MDOKG5A>
2022-06-20 18:34:11 +0900

    1522525

--- GitHubProject1/src/com/sist/MainClass.java
+++ GitHubProject1/src/com/sist/MainClass.java
@@ -4,11 +4,9 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git");
-		System.out.println("Hello Git");
-		System.out.println("Hello Git");
-		System.out.println("Hello Git");
-		System.out.println("Hello Git");
+		System.out.println("수정");
+		System.out.println("수정");
+
 
 	}
 
