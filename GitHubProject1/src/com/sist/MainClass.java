8de31fbb7c3fc489ab54dd819e709019821c63eb
a <a@DESKTOP-MDOKG5A>
2022-06-20 18:26:47 +0900

    수정

--- GitHubProject1/src/com/sist/MainClass.java
+++ GitHubProject1/src/com/sist/MainClass.java
@@ -7,6 +7,8 @@ public class MainClass {
 		System.out.println("Hello Git");
 		System.out.println("Hello Git");
 		System.out.println("Hello Git");
+		System.out.println("Hello Git");
+		System.out.println("Hello Git");
 
 	}
 
